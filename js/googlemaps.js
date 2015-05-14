25b3a6472de5920905262c0bc4c28398509bb77c
Yankel Lahiany <yankel@lahiany.me>
2015-05-14 15:08:31 -0400

    Contact form small changes

--- js/googlemaps.js
+++ js/googlemaps.js
@@ -4,7 +4,7 @@ window.marker = null;
 function initialize() {
 	var map;
 
-	var mymap = new google.maps.LatLng(51.476853, -0.0005);
+	var mymap = new google.maps.LatLng(45.606649, -73.712409);
 
 	var style = [ 
 		{ "featureType": "road", 
